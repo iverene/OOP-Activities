48f9bb267cacd7139d0e35ef101a70bf458221f8
iverene <causapiniverenegrace@gmail.com>
2024-09-12 15:04:53 +0800

    first commit

---
+++ Activity.java
@@ -0,0 +1,12 @@
+class Activity {
+    public static void main (String args[]) {
+
+       Dog dog = new Dog();
+       dog.name = "Sky";
+       dog.numPuppies = dog.puppies[];
+
+       System.out.println(dog.showName());
+       System.out.println(dog.bark());
+       System.out.println(dog.numberPuppies());
+    }
+}
