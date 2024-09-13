8f3f8af32bbf8b0970fb2b1d7bb6b48c5c889c28
iverene <causapiniverenegrace@gmail.com>
2024-09-13 13:44:54 +0800

    Update Activity.java

--- Activity.java
+++ Activity.java
@@ -4,10 +4,6 @@ class Activity {
        Dog dog = new Dog();
        dog.setName("Skye");
        dog.setNumberOfPuppies(3);
-       
-       dog.puppies[0] = "Chase";
-       dog.puppies[1] = "Zuma";
-       dog.puppies[2] = "Rubble";
 
        System.out.println(dog.showName());
        System.out.println(dog.bark());
