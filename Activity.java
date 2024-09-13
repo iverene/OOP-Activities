d50ca34aff5247dbf5ab4d4ff788391fdee6bbae
iverene <causapiniverenegrace@gmail.com>
2024-09-13 13:43:27 +0800

    Update Activity.java

--- Activity.java
+++ Activity.java
@@ -2,11 +2,20 @@ class Activity {
     public static void main (String args[]) {
 
        Dog dog = new Dog();
-       dog.name = "Sky";
-       dog.numPuppies = dog.puppies[];
+       dog.setName("Skye");
+       dog.setNumberOfPuppies(3);
+       
+       dog.puppies[0] = "Chase";
+       dog.puppies[1] = "Zuma";
+       dog.puppies[2] = "Rubble";
 
        System.out.println(dog.showName());
        System.out.println(dog.bark());
-       System.out.println(dog.numberPuppies());
+       System.out.println(dog.showNumberOfPuppies());
+       System.out.println("Puppies:");
+       
+       for (int i=0; i < dog.showPuppies().length; i++) {
+       System.out.println(dog.showPuppies()[i]);
+    }
     }
 }
