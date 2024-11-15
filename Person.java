fd8f75efd79611a22cd3aaa30376b08c20a4bbce
iverene <causapiniverenegrace@gmail.com>
2024-11-15 13:51:51 +0800

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,20 @@
+class Person {
+    protected String name;
+    protected String address;
+    
+    public void setname (String name) {
+        this.name = name;
+    }
+    
+    public void setaddress (String address) {
+        this.address = address;
+    }
+    
+    public String getname() {
+        return name;
+    }
+    
+    public String getaddress() {
+        return address;
+    }
+}
