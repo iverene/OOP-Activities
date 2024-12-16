9e60ad428a0d55ae96b2e86a0f8bdf197eff1ca3
iverene <causapiniverenegrace@gmail.com>
2024-12-16 11:10:13 +0800

    Update Dog.java

--- Dog.java
+++ Dog.java
@@ -1,40 +1,50 @@
 class Dog {
-    String names;
+    String name;
     boolean isMale;
     int numPuppies;
-    Dog[] puppies = new Dog[2];
-    Dog puppy;
-    
+    Dog[] puppies = new Dog[5];
+
+
     
     public void setName(String name) {
-        names = name;
+        this.name = name;
     }
 
+    
     public String showName() {
-        return "Name of the dog: " + names ;
+        return "Name of the dog: " + name;
     }
 
+   
     public String bark() {
         return "Woof! Woof!";
     }
+
     
-    public void setNumberOfPuppies() {
-        numPuppies = puppies.length;
+    public void setNumberOfPuppies(int numPuppies) {
+        this.numPuppies = numPuppies;
+        this.puppies = new Dog[numPuppies];
     }
 
-    public void setPuppy(Dog puppies) {
-        puppy = puppies;
-    }
-
+    
     public String showNumberOfPuppies() {
         return "Number of puppies: " + numPuppies;
     }
+
     
-    public Dog[] showPuppies() {
-        return puppies;
+    public void addPuppy(int index, Dog puppy) {
+        if (index >= 0 && index < puppies.length) {
+            this.puppies[index] = puppy;
+        } 
     }
 
-    public void setPuppies(Dog puppies[]) {
-        puppies=puppies;
+   
+    public void showPuppies() {
+        System.out.println("Puppies:");
+        for (Dog puppy : puppies) {
+            if (puppy != null) {
+                System.out.println(puppy.name);
+            }
+        }
     }
 }
